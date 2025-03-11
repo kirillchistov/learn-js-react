493dee990bb363f5a6e38607555253ce3178554b
Kirill Chistov <kirill.v.chistov@yandex.ru>
2025-03-11 15:24:02 +0300

    hw-2 remove extras

--- vite-react/src/main.jsx
+++ vite-react/src/main.jsx
@@ -1,13 +1,9 @@
-// import { createElement } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { restaurants } from './materials/mock';
-// import App from './App.jsx';
 
 const root = document.getElementById('root');
 const reactRoot = createRoot(root);
-
-// console.log(restaurants);
 
 reactRoot.render(
   <div>
@@ -15,7 +11,7 @@ reactRoot.render(
       restaurants.map((restaurant) => (
         <div key={restaurant.id} className='divRed'>
           <h2 className='headGreen'>{restaurant.name}</h2>
-          <h3 className='headBlue'>Меню</h3>
+          <h3 className='headBlue'>Menu</h3>
           <ul>
             {
               restaurant.menu.map((item) => (
@@ -23,7 +19,7 @@ reactRoot.render(
               ))
             }
           </ul>
-          <h3 className='headRed'>Обзоры</h3>
+          <h3 className='headRed'>Reviews</h3>
           <ul>
             {
               restaurant.reviews.map((review) => (
@@ -36,34 +32,3 @@ reactRoot.render(
     }
   </div>
 )
-
-
-
-
-
-// reactRoot.render(
-//   <div>
-//     {restaurants.map((place) => (
-//       <div key={place.id}>
-//         <h2>{place.name}</h2>
-//         console.log(place.id, place.name);
-//         <h3>Меню</h3>
-//         <ul className='ulGreen'>
-//           {restaurants.menu.map((item) => (
-//             <li key={item.id}>
-//               {item.name}
-//             </li>
-//           ))}
-//         </ul>
-//         <h3>Отзывы</h3>
-//         <ul className='ulBlue'>
-//           {restaurants.reviews.map((review) => (
-//             <li key={review.id}>
-//               {review.text}
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     ))}
-//   </div>
-// );
