932b040e04bd744f5be8773292115e949fcf7565
Kirill Chistov <kirill.v.chistov@yandex.ru>
2025-04-08 19:07:02 +0300

    hw-8 fix layout

--- vite-react/src/pages/home-page.jsx
+++ vite-react/src/pages/home-page.jsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router';
 import { Modal } from '../components/Modal/modal';
+import heroImage from '../assets/images/hero-image.jpg';
+import styles from './home.module.css';
 
 export const HomePage = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <h2>Home Page</h2>
       <Link to='/restaurants'>All restaurants</Link>
       {/* <Link to='/reviews'>All reviews</Link>
@@ -11,6 +13,11 @@ export const HomePage = () => {
       <Modal> 
         <div>modal</div>
       </Modal>
+      <img 
+        className={styles.heroImage}
+        src={heroImage} 
+        alt='Restaurants of New Orleans' 
+      />
     </div>
   );
 };
